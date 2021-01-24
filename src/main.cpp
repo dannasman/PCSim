ead850cf79ef52c2175d8ba4861c1d13ed8a5577
Dan Nasman <dan.nasman@gmail.com>
2021-01-24 17:31:38 +0200

    removed unused firework class added third particle to test simulation in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@ int main()
 {
     Particle particle1;
     Particle particle2;
+    Particle particle3;
     Particle helper;
 
     particle1.setPosition(0.0, 0.0, 0.0);
@@ -25,23 +26,43 @@ int main()
     particle2.setDamping(1.0);
     particle2.setRadius(5.0);
 
+    particle3.setPosition(0.0, 0.0, 20);
+    particle3.setVelocity(0.0, 4.99, -0.2);
+    particle3.setAcceleration(0.0, -0.981, 0.0);
+    particle3.setMass(0.1);
+    particle3.setDamping(1.0);
+    particle3.setRadius(5.0);
+
     ofstream ParticleData1("./particle1.csv");
     ofstream ParticleData2("./particle2.csv");
+    ofstream ParticleData3("./particle3.csv");
 
     for (float i = 1; i <= 15; i++)
     {
         particle1.integrate(i);
         particle2.integrate(i);
-        helper = particle1;
+        particle3.integrate(i);
+
+        Particle helper1 = particle1;
+        Particle helper2 = particle2;
+
         particle1.checkForCollision(particle2);
-        particle2.checkForCollision(helper);
+        particle1.checkForCollision(particle3);
+
+        particle2.checkForCollision(helper1);
+        particle2.checkForCollision(particle3);
+
+        particle3.checkForCollision(helper1);
+        particle3.checkForCollision(helper2);
 
         ParticleData1 << particle1.position.x << ", " << particle1.position.y << ", " << particle1.position.z << "\n";
         ParticleData2 << particle2.position.x << ", " << particle2.position.y << ", " << particle2.position.z << "\n";
+        ParticleData3 << particle3.position.x << ", " << particle3.position.y << ", " << particle3.position.z << "\n";
     }
 
     ParticleData1.close();
     ParticleData2.close();
+    ParticleData3.close();
 
     return 0;
 }
--- src/plot.py
+++ src/plot.py
@@ -5,10 +5,12 @@ import pandas as pd
 
 data1 = pd.read_csv("./particle1.csv")
 data2 = pd.read_csv("./particle2.csv")
+data3 = pd.read_csv("./particle3.csv")
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection = '3d')
 for i in range(len(data1.values)):
     ax.plot(data1.values[i][0], data1.values[i][1], data1.values[i][2], c='b', marker = 'x')
     ax.plot(data2.values[i][0], data2.values[i][1], data2.values[i][2], c='r', marker = 'x')
+    ax.plot(data3.values[i][0], data3.values[i][1], data3.values[i][2], c='r', marker = 'x')
 plt.show()
